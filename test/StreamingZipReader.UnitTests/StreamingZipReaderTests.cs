8c2fa0bace48c56b66cc17b1295222f2e0a7b61b
Björn Harrtell <bjorn@septima.dk>
2024-04-28 19:45:50 +0200

    Implement basic test

--- test/StreamingZipReader.UnitTests/StreamingZipReaderTests.cs
+++ test/StreamingZipReader.UnitTests/StreamingZipReaderTests.cs
@@ -1,10 +1,27 @@
-namespace StreamingZipReader.UnitTests;
+using System.IO.Compression;
 
-public class UnitTest1
+namespace Wololo.StreamingZipReader.UnitTests;
+
+public class StreamingZipReaderTests
 {
     [Fact]
-    public void Test1()
+    public async Task BasicTest()
     {
-
+        var directoryInfo = Directory.CreateTempSubdirectory();
+        var stream = new MemoryStream();
+        ZipFile.CreateFromDirectory(directoryInfo.FullName, stream);
+        stream.Position = 0;
+        var zipArchive = new ZipArchive(stream, ZipArchiveMode.Create, true);
+        var entry = zipArchive.CreateEntry("test");
+        var writeStream = entry.Open();
+        byte[] bytes = new byte[1024];
+        Random.Shared.NextBytes(bytes);
+        writeStream.Write(bytes);
+        writeStream.Dispose();
+        zipArchive.Dispose();
+        stream.Position = 0;
+        var reader = new StreamingZipReader(stream);
+        await reader.MoveToNextEntryAsync(true, CancellationToken.None);
+        Assert.Equal((uint) 1024, reader.CurrentEntry.Length);
     }
 }
